c1a1fd103717e7317cb31bf86421b0c1e6ca8d36
Sylvestre Ho <sylvestre.business@gmail.com>
2017-03-03 18:58:00 +0100

    + service conf

---
+++ module/Blog/config/service.config.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Blog;
+
+return [
+  'invokables' => [
+    'Blog\Repository\PostRepository' => 'Blog\Repository\PostRepositoryImpl'
+  ],
+  'factories' => [
+    'Blog\Service\BlogService' => function(\Zend\ServiceManager\ServiceManager $sm) {
+        $blogService = new \Blog\Service\BlogServiceImpl();
+        $blogService->setPostRepository($sm->get('Blog\Repository\PostRepository'));
+
+        return $blogService;
+    }
+  ],
+  'initializers' => [
+    function (\Zend\ServiceManager $sm, $instance) {
+      if ($instance instanceof \Zend\Db\Adapter\AdapterAwareInterface) {
+        $instance->setDbAdapter($sm->get('Zend\Db\Adapter\Adapter'));
+      }
+    }
+  ]
+];
