30643ec9c05828d1e0c05adf6bb54c3f4b09ffda
Sylvestre Ho <sylvestre.business@gmail.com>
2017-05-15 16:22:23 +0200

    +module.config.php

---
+++ module/User/config/module.config.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace User;
+
+return [
+  'router' => [
+    'routes' => [
+      'add_user' => [
+        'type' => 'Literal',
+        'options' => [
+          'route' => '/user/add',
+          'defaults' => [
+            'controller'  => 'User\Controller\Index',
+            'action'      => 'add'
+          ]
+        ]
+      ]
+    ]
+  ],
+  'controllers' => [
+    'factories' => [
+      'User\Controller\Index' => 'User\Controller\IndexControllerFactory'
+    ]
+  ],
+  'view_manager' => [
+    'template_path_stack' => [
+      __DIR__ . '/../view'
+    ]
+  ]
+];
